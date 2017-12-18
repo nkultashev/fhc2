be81cea170dc310d3cfcc0e07e0b9b2decd3ab93
Nikolay Kultashev <nkultashev@ru.ibm.com>
2017-12-18 19:49:17 +0300

    Comments added

--- src/main/lua/PumpsOnOff.lua
+++ src/main/lua/PumpsOnOff.lua
@@ -6,16 +6,25 @@
 %% globals
 HeatingDayMode
 --]]
+-- Above is a control header for a Fibaro HC2 engine.
+-- % properties - trigger the scene when a device property changed.
+-- % globals - trigger the scene when a global variable value changed.
 
-local dayTempToKeep = 20
-local nightTempToKeep = dayTempToKeep - 1
-local temp = tonumber(fibaro:getValue(197, "value")) -- Текущая температура
+local thermDeviceId = 197                 -- Id of the thermometer device
+local dayTempToKeep = 20                  -- Temp. to maintain when DAY mode 
+local nightTempToKeep = dayTempToKeep - 1 -- Temp. to maintain when NIGHT mode 
 
-SendToPumps = function(cmd)
-  fibaro:call(23, cmd); -- Насос: ДОМ
-  fibaro:call(25, cmd); -- Насос: БАК
+--
+-- SendCommand: Send a command to controlled devices
+--
+SendCommand = function(cmd)
+  fibaro:call(23, cmd); -- Pump: HOME
+  fibaro:call(25, cmd); -- Pump: HEAT. ACC.
 end
 
+--
+-- Functions to write color text on the debug console
+--
 Color = function(color, message)
   return string.format('<span style="color:%s;">%s</span>', color, message)
 end
@@ -40,13 +49,24 @@ Yellow = function(message)
   return Color('yellow', message)
 end
 
+--
+-- Debug: a shortcut to fibaro:debug(...) function
+--
 Debug = function(message)
   fibaro:debug(message)
 end
 
-local tempToKeep
+--
+-- MAIN PROCEDURE
+--
+-- Get current temp. from the declared thermometer...
+local temp = tonumber(fibaro:getValue(thermDeviceId, "value")) 
+
+-- Get current mode from the global variable HeatingDayMode...
 local heatingDayMode = (fibaro:getGlobalValue("HeatingDayMode") == "True")
 
+-- Save required temp. to the tempToKeep variable and report the current mode...
+local tempToKeep
 if heatingDayMode then
   Debug('Mode: ' .. Yellow('DAY'))
   tempToKeep = dayTempToKeep
@@ -55,15 +75,17 @@ else
   tempToKeep = nightTempToKeep
 end
 
+-- Report some info...
 Debug('Current temp: ' .. temp)
 Debug('Temp to maintain: ' .. tempToKeep)
 
+-- Send on/off command to the controlled devices depending on the current temp...
 if temp < tempToKeep then
   Debug('Switching pumps ' .. Green('ON'))
-  SendToPumps("turnOn");
+  SendCommand("turnOn");
 elseif temp > tempToKeep then
   Debug('Switching pumps ' .. Red('OFF'))
-  SendToPumps("turnOff");
+  SendCommand("turnOff");
 end
 
 Debug(Gray('---------------------'))
